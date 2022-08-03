56afc023983b247273f17fe2dbf50ad9962cd33d
ArcoKim <mcblueboyk@naver.com>
2022-08-03 15:07:30 +0900

    project-6 finish!

---
+++ project-7/script/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".menubar > li").hover(function() {
+        $(this).find(".submenu").stop().slideDown(400)
+    }, function() {
+        $(".submenu").stop().slideUp(400)
+    })
+    setInterval(function() {
+        $(".slides").animate({"margin-top":"-300px"}, function() {
+            $(".slide:first").appendTo(".slides")
+            $(".slides").css({"margin-top":"0px"})
+        })
+    }, 3000)
+})
